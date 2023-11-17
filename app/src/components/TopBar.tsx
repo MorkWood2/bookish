e941d74da0315d3279ee60fe0a89bbff7041afaa
Mark Wild <mark.wild189@gmail.com>
2023-11-17 15:35:25 -0800

    fix: appbar styling

--- app/src/components/TopBar.tsx
+++ app/src/components/TopBar.tsx
@@ -1,13 +1,58 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import SearchBar from './SearchBar';
+import InputBase from '@mui/material/InputBase';
 import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
 
 interface TopBarProps {
   drawerWidth: number;
 }
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 export default function TopBar(props: TopBarProps) {
   const { drawerWidth } = props;
@@ -23,19 +68,59 @@ export default function TopBar(props: TopBarProps) {
     >
       <Toolbar>
         <IconButton
-          color='default'
-          aria-label='open drawer'
+          size='large'
           edge='start'
-          //   onClick={handleDrawerToggle}
-          sx={{ mr: 2, display: { sm: 'none' } }}
+          color='inherit'
+          aria-label='open drawer'
+          sx={{ mr: 2 }}
         >
           <MenuIcon />
         </IconButton>
-        <Typography variant='h6' noWrap component='div'>
-          Responsive drawer
+        <Typography
+          variant='h6'
+          noWrap
+          component='div'
+          sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+        >
+          BOOKISH
         </Typography>
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder='Search…'
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </Search>
       </Toolbar>
-      <SearchBar></SearchBar>
     </AppBar>
+    // <AppBar
+    //   position='fixed'
+    //   component='div'
+    //   sx={{
+    //     width: { sm: 1 },
+    //     ml: { sm: `${drawerWidth}px` },
+    //     zIndex: (theme) => theme.zIndex.drawer + 1,
+    //   }}
+    // >
+    //   <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+    //     <div style={{ display: 'flex', alignItems: 'center' }}>
+    //       <IconButton
+    //         color='default'
+    //         aria-label='open drawer'
+    //         edge='start'
+    //         //   onClick={handleDrawerToggle}
+    //         sx={{ mr: 2, display: { sm: 'none' } }}
+    //       >
+    //         <MenuIcon />
+    //       </IconButton>
+    //       <Typography variant='h6' noWrap component='div'>
+    //         Responsive drawer
+    //       </Typography>
+    //     </div>
+    //   </Toolbar>
+    //   <SearchBar></SearchBar>
+    // </AppBar>
   );
 }
